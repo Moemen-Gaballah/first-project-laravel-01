6febfd51495f51dc24ec5900b6f292723d145f71
moemen_gaballah <moemengaballa@gmail.com>
2018-05-03 10:26:47 +0200

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('main')
+
+@section('title', 'About Me')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1>About Me</h1>
+            <p class="lead">My name: <b>Mo'men ُEssam Gaballah.</b> <br> age: <b>21</b> years. <br> study: <b>Web Developer</b> online courses and Faculty of commerce menofia university. </p>
+        </div>
+    </div> <!-- end of header .row -->
+@endsection
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,2 @@
+<hr>
+<p class="text-center">Copyright Mo'men Gaballah - All Rights Reserved</p>
